761e9c432e1fe11adf72fab079e1df854c5e1fe4
ZION <leedjcap@gmail.com>
2024-04-11 00:33:29 +0900

    test check

---
+++ app/src/main/java/com/example/apitest/SubwayAPI.kt
@@ -0,0 +1,11 @@
+package com.example.apitest
+
+interface SubwayAPI {
+//    @GET("api.odcloud.kr/api")
+//    fun getCongestionData(
+//        @Header("appKey") appKey: String,
+//        @Query("line") line: String,
+//        @Query("stationName") stationName: String
+//
+//    )
+}
